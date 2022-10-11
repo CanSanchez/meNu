77c9f5e302a8939dcc76ed9340ad26afa5511d71
shaelorch <slorch@my.bcit.ca>
2022-10-11 15:48:20 -0700

    button component

--- App.js
+++ App.js
@@ -6,13 +6,19 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { CalendarSimpleUsageShowcase } from './components/calendar';
 import { BottomNavigationSimpleUsageShowcase } from './components/navbar';
 import { InputSimpleUsageShowcase } from './components/form';
+import { ButtonMain} from './components/button';
+
+
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text category='h1' style={{paddingBottom: '5%'}}>Calendar</Text>
     <CalendarSimpleUsageShowcase />
     <InputSimpleUsageShowcase />
-    <InputSimpleUsageShowcase label='Last Name' />
+    <ButtonMain> 
+      
+    </ButtonMain>
+    
   </Layout>
 );
 

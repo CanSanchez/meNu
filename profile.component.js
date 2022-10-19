eef1a154c431779a1a4693cc0cee4889993d20da
msyukurrr <msyukur@my.bcit.ca>
2022-10-19 14:28:08 -0700

    naming changed

--- profile.component.js
+++ profile.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
 import { Text, Divider, Layout, TopNavigation, Icon } from '@ui-kitten/components';
-import { AvatarProfile } from './components/avatar';
-import { ButtonMain } from './components/button';
-import { PopupCardButton } from './components/popupcard';
+import { AvatarProfile } from './components/Avatar';
+import { ButtonMain } from './components/Button';
+import { PopupCardButton } from './components/PopupCard';
 // import { StyleSheet, View } from 'react-native';
 
 export const ProfileScreen = () => {

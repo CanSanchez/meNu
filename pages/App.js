3fe4025e6c4c174f14cce76a32dc817a39c33bae
CanSanchez <asanchez24@my.bcit.ca>
2022-10-22 00:23:38 -0700

    revert back to a3fc5f56188017aa9cf7c04027930599ca122fb8. Cannot debug errors from current commits

--- pages/App.js
+++ pages/App.js
@@ -3,7 +3,6 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from '../components/NavBar';
-import styled from 'styled-components';
 
 export default () => (
   <>

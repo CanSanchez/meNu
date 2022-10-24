aa29dcb0f531a5a67751c5b61098a77fc8ca4c83
shaelorch <slorch@my.bcit.ca>
2022-10-24 14:07:14 -0700

    added updated homepage

--- pages/home.component.js
+++ pages/home.component.js
@@ -17,23 +17,28 @@ export const HomeScreen = () => {
   });
 
   return (
-    <SafeAreaView style={{ flex: 1}}>
-      <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFEF4' }}>
-        <HeaderTitle />
-        <TutorialCard style={styles.card}/>
-        <InputSimpleUsageShowcase />
-        <ButtonMain />   
-      </Layout>
-    </SafeAreaView>
-  );
-};
+
+    
+    <>
+      <Layout style={styles.layout}>
+        <HeaderTitle headertext='Welcome back, Jennifer!' />
+        <Image style={{ width: 69, height: 60, margin: 10 }} source={require('../assets/Bearface.png')} />
+        <TextCard />
+        <RecActivities />
+        <Reminders />
+      </Layout></>
+
+)
+
+}
 
 const styles = StyleSheet.create({
   layout:{
     flex: 1,
     alignItems:'center',
+    paddingTop:30
+
+
   }
   
 })
-

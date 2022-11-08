7b240cdd5b310988996d8b65249f6d7bbf2bde4b
msyukurrr <msyukur@my.bcit.ca>
2022-11-08 12:44:19 -0800

    activity card

---
+++ components/ActivityCard/index.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Image, View} from 'react-native';
+import React from 'react';
+import { Card, Text } from '@ui-kitten/components';
+import { FlexStyleProps } from '@ui-kitten/components/devsupport';
+
+export const ActivityCard = ({
+    fronttext="Pilates",
+    func=console.log('not a button')
+}) => {
+
+    return (
+        <Card style={styles.acard} onPress={func}>
+            <Image style={styles.picture}
+            source={require('../../assets/pilates.png')}></Image>
+            <Text category='h5' style={styles.text}>{fronttext}</Text>
+        </Card>
+    )
+}
+
+const styles = StyleSheet.create({
+    acard: {
+        width: 300,
+        height: 400,
+        display:"flex",
+        padding: 20,
+        borderRadius:30,
+        backgroundColor:"#FFF3D3",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        textAlign:'center',
+        marginTop:15,
+    },
+})

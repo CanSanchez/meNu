201a89a782ce14a348af33d35b174307833115aa
shaelorch <slorch@my.bcit.ca>
2022-11-10 14:15:24 -0800

    made calendar pop up

--- pages/calendar.component.js
+++ pages/calendar.component.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
-import { Button, Divider, Layout, Icon } from '@ui-kitten/components';
+import { Button, Divider, Layout, Icon, View } from '@ui-kitten/components';
 import { CalendarSimpleUsageShowcase } from '../components/Calendar';
 import { Reminders } from '../components/Reminders';
 import { CalendarCard } from '../components/CalendarCard';
 import { TopNavigationSimpleUsageShowcase } from '../components/TopNav';
 import { ButtonMain } from '../components/Button';
+import { useState } from 'react';
 
 const PlusIcon = (props) => (
   <Icon {...props} name='plus-circle-outline' fill="#434343" />
@@ -13,6 +14,8 @@ const PlusIcon = (props) => (
 
 
 export const CalendarScreen = () => {
+
+  const [open, setOpen] = useState(false);
 
   return (
     <ScrollView scrollEventThrottle={200}>
@@ -24,16 +27,23 @@ export const CalendarScreen = () => {
             
       <ScrollView>
         <TopNavigationSimpleUsageShowcase />
-      <Layout style={{paddingTop:50}}>
-      <ButtonMain func={() => setVisible(true)} text={btntxt} stat='danger' sz='tiny'/>
-        <Icon name="plus-circle-outline" fill="#434343" style={{width:23, height:23, paddingLeft:700}} />
-      </Layout>
-        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Layout style={{paddingTop:40}}>
         
-          <CalendarSimpleUsageShowcase />
+          <Icon onPress={()=>setOpen(true)} name="plus-circle-outline" fill="#434343" style={{width:23, height:23, paddingLeft:725, marginBottom:15}} />
         </Layout>
-        <Reminders />
-        <CalendarCard/>
+        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          
+            <CalendarSimpleUsageShowcase />
+        
+           {
+           open?<CalendarCard />:null
+           }
+       
+            
+        
+        </Layout>
+      <Reminders />
+        
       </ScrollView>
     </SafeAreaView>
     </Layout>
